8d03d69f16704b590422a9dfcf7fc6c8b6624fba
Greg Heitz <greg.heitz@userreplay.com>
2016-08-01 19:25:10 +0100

    code update

--- src/DataProvider.php
+++ src/DataProvider.php
@@ -13,63 +13,57 @@ use Codeception\Exception\TestParseException;
 class DataProvider extends \Codeception\Platform\Extension
 {
     // list events to listen to
-    public static $events = array(
-    //run before any test`
-    'suite.before' => 'before'
-    );
+    public static $events = ['suite.before' => 'before'];
 
     public function before(\Codeception\Event\SuiteEvent $se)
     {
-    $suite = $se->getSuite();
-    $tests = $suite->tests();
-    foreach ($tests as $id => $test) {
-        if (get_class($test) == 'Codeception\Test\Cest') {
-            $testClass = $test->getTestClass();
-            $testMethod = $test->getTestMethod();
-            $testFile = $test->getMetadata()->getFilename();
-            $testActor = $test->getMetadata()->getCurrent('actor');
-            $dataMethod = Annotation::forMethod($testClass, $testMethod)->fetch('dataprovider');
-            if (false === empty($dataMethod)) {
-                if (false === is_callable([$testClass, $dataMethod])) {
-                    throw new TestParseException(
-                        $testFile, "DataProvider {$dataMethod} for ${testClass}->${testMethod} is invalid or not callable"
-                        . PHP_EOL .
-                        "Make sure this is a public static function."
-                    );
-                }
-                try {
-                    $dataProvider = new \PHPUnit_Framework_TestSuite_DataProvider();
-                    $examples = $testClass::$dataMethod();
-                    foreach ($examples as $example) {
-                        if ($example === null) {
-                            throw new TestParseException(
-                                $testFile, "Values return by DataProvider {$dataMethod} for ${testClass}->${testMethod} is invalid"
-                            );
+        $suite = $se->getSuite();
+        $tests = $suite->tests();
+        foreach ($tests as $id => $test) {
+            if (is_a($test, 'Codeception\Test\Cest')) {
+                $testClass = $test->getTestClass();
+                $testClassName = get_class($testClass);
+                $testMethod = $test->getTestMethod();
+                $testFile = $test->getMetadata()->getFilename();
+                $testActor = $test->getMetadata()->getCurrent('actor');
+                $dataMethod = Annotation::forMethod($testClass, $testMethod)->fetch('dataprovider');
+                if (false === empty($dataMethod)) {
+                    try {
+                        if (false === is_callable([$testClass, $dataMethod])) {
+                            throw new Exception();
                         }
-                        $dataTest = new CestFormat($testClass, $testMethod, $testFile);
-                        $dataTest->getMetadata()->setServices([
-                            'di'         => $test->getMetadata()->getService('di'),
-                            'dispatcher' => $test->getMetadata()->getService('dispatcher'),
-                            'modules'    => $test->getMetadata()->getService('modules')
-                        ]);
-                        $dataTest->getMetadata()->setCurrent(['actor' => $testActor, 'example' => $example]);
-                        $step = new Comment('', $dataTest->getMetadata()->getCurrent('example'));
-                        $dataTest->getScenario()->setFeature($dataTest->getSpecFromMethod() . ' | ' . $step->getArgumentsAsString(100));
-                        $groups = Annotation::forMethod($testClass, $testMethod)->fetchAll('group');
-                        $dataProvider->addTest($dataTest, $groups);
+                        $dataProvider = new \PHPUnit_Framework_TestSuite_DataProvider();
+                        $examples = $testClassName::$dataMethod();
+                        foreach ($examples as $example) {
+                            if ($example === null) {
+                                throw new TestParseException(
+                                    $testFile, "Invalid values format returned by DataProvider {$dataMethod} for ${$testClassName}->${testMethod}."
+                                );
+                            }
+                            $dataTest = new CestFormat($testClass, $testMethod, $testFile);
+                            $dataTest->getMetadata()->setServices([
+                                'di'         => $test->getMetadata()->getService('di'),
+                                'dispatcher' => $test->getMetadata()->getService('dispatcher'),
+                                'modules'    => $test->getMetadata()->getService('modules')
+                            ]);
+                            $dataTest->getMetadata()->setCurrent(['actor' => $testActor, 'example' => $example]);
+                            $step = new Comment('', $dataTest->getMetadata()->getCurrent('example'));
+                            $dataTest->getScenario()->setFeature($dataTest->getSpecFromMethod() . ' | ' . $step->getArgumentsAsString(100));
+                            $groups = Annotation::forMethod($testClass, $testMethod)->fetchAll('group');
+                            $dataProvider->addTest($dataTest, $groups);
+                        }
+                        $tests[$id] = $dataProvider;
+                    } catch (\Exception $e) {
+                        throw new TestParseException(
+                            $testFile, "DataProvider {$dataMethod} for ${$testClassName}->${testMethod} is invalid or not callable."
+                            . PHP_EOL .
+                            "Make sure this is a public static function."
+                        );
                     }
-                    $tests[$id] = $dataProvider;
-                } catch (\Exception $e) {
-                    throw new TestParseException(
-                        $testFile, "DataProvider {$dataMethod} for ${testClass}->${testMethod} is invalid or not callable"
-                        . PHP_EOL .
-                        "Make sure this is a public static function."
-                    );
                 }
             }
         }
-    }
-    $suite->setTests($tests);
+        $suite->setTests($tests);
     }
 
 }
